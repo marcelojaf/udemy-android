e9f0fa6b9432c3144bfd67c72f7dd206f09b94b1
Marcelo Amador <marcelojaf@gmail.com>
2023-07-04 17:27:21 -0300

    Included list view

--- 07-PermanentDataStorage/Notes/app/src/main/java/com/cklabs/notes/MainActivity.java
+++ 07-PermanentDataStorage/Notes/app/src/main/java/com/cklabs/notes/MainActivity.java
@@ -3,12 +3,23 @@ package com.cklabs.notes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> notes = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView lsvNotes = findViewById(R.id.lsvNotes);
+        notes.add("Example Note");
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
+        lsvNotes.setAdapter(arrayAdapter);
     }
 }
